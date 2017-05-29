29dc37c0f158e736d9393350090418493e41c82e
Awemany <awemany@protonmail.com>
2017-05-29 20:18:51 +0200

    Update for 2nd ad-hoc member list import

--- import_members_from_csv.py
+++ import_members_from_csv.py
@@ -7,7 +7,7 @@ from butype import *
 from butypes import *
 from appmaker import make_app
 
-app, db=make_app(fresh=True)
+app, db=make_app(fresh=False)
 
 bu_developer="theZerg"
 bu_president="solex"
@@ -29,29 +29,52 @@ for row in csv.reader(csv_file):
 
         nick = (row[1]
                 .replace(" ", "_") # sorry ...
-                )
+                ).replace(".", "_") # and sorry again ..
 
         joined = date2epoch(row[3])
         app_text = row[4]
 
-        addr = row[5]
+        addr = row[5].strip()
         bitcoin.b58check_to_hex(addr)
         
         last_vote = date2epoch(row[6])
 
         last_action = max(joined, last_vote)
 
-        print("Adding:", nick, addr)
-        if 1:
+        print("Adding:", nick, addr, "?")
+
+        while True:
+            m = Member.by_name(nick)
+            b = Member.by_address(addr)
+            if m is not None:
+                print("by name:", m.name, m.address)
+
+            if b is not None:
+                print("by address:", b.name, b.address)
+
+            if m != b:
+                print("XXXX Change in address, adding _!!")
+                nick+="_"
+            else:
+                break
+            
+        if m is None and b is None:
+            print("add!")
             m = Member(nick, addr)
             Global.set_member_last_vote_time(m, last_action)
             members.append(m)
             db.session.add(m)
             db.session.commit()
-            
+        else:
+            m = Member.by_name(nick)
+            Global.set_member_last_vote_time(m, last_action)
+            members.append(m)
+            print("Not adding %s as in db already" % m.name)
+            db.session.commit()
+                        
         entries.append((nick, addr, last_action))
     except Exception as e:
-        print ("Missing:", row[1])
+        print ("XXXX Not enough data:", row[1], e)
 
 if 1:
     developer=Member.by_name(bu_developer)
