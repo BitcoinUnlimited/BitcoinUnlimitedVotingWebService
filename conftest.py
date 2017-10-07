f4590b896e0e888e780236dfcb0c93b31f61a9b1
Awemany <awemany@protonmail.com>
2017-10-07 18:19:09 +0200

    Disable DEBUG level logging for gnupg module (too verbose)

--- conftest.py
+++ conftest.py
@@ -1,8 +1,11 @@
 import pytest
 import config
+import gnupg
+import logging
 
 # global tweaks for all pytesting
 @pytest.fixture(scope="session", autouse=True)
 def tweak_global_config():
     config.disable_signature_checking = False
     
+    gnupg.logger.setLevel(logging.INFO)
