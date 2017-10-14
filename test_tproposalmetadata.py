acdae1b035927ecec381b8babf30f2bfd7d65b02
Awemany <awemany@protonmail.com>
2017-10-14 13:19:08 +0200

    test_tproposalmetadata fix: Use test session
    This test used the real DB for testing before.

--- test_tproposalmetadata.py
+++ test_tproposalmetadata.py
@@ -4,18 +4,18 @@ from butype import *
 import butypes
 import tproposalmetadata
 from test_tmemberlist import makeTestMemberList, makeTestKey
+from test_helpers import bare_session
 from jvalidate import ValidationError
 
 
-def test_invalid_construction1():
+def test_invalid_construction1(bare_session):
     name, addr = "fakemember", makeTestKey("fakemember")[1]
     member = tmember.Member(name, addr)
 
     assert not butypes.is_used(member)
-    
+
     with pytest.raises(ValidationError):
         rf = member
         md = tproposalmetadata.ProposalMetadata("test.pdf",
                                                 "application/pdf",
                                                 rf, None)
-        
